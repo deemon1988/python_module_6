ff20005a8e63ff6ca0b3ff3cdba149d9af0ab1ac
deemon1988 <dmn72835@gmail.com>
2024-11-25 20:19:00 +0300

    Задание "Они все так похожи" - setter for radius, getter overwrite, mangling for __private attributes

--- module6hard.py
+++ module6hard.py
@@ -7,7 +7,7 @@ class Figure:
     sides_count = 0
 
     def __init__(self, color, *sides):
-        self.__sides = list(sides)
+        self.__sides = self.initialize_sides(sides)
         self.__color = list(color)
         self.filed = False
 
@@ -28,7 +28,7 @@ class Figure:
     def __is_valid_sides(self, *sides):
         is_valid = True
         for side in list(*sides):
-            if side < 0 and not isinstance(side, int):
+            if side < 0 or not isinstance(side, int):
                 is_valid = False
         return len(sides) == self.sides_count and is_valid
 
@@ -36,11 +36,17 @@ class Figure:
         return self.__sides
 
     def __len__(self):
-        return sum(self.__sides)
+        return sum(self.get_sides())
 
     def set_sides(self, *new_sides):
         if self.__is_valid_sides(new_sides):
             self.__sides = list(new_sides)
+
+    def initialize_sides(self, *sides):
+        if len(sides) > 1 and len(sides) != self.sides_count:
+            return [1] * self.sides_count
+        else:
+            return list(*sides) * self.sides_count
 
 
 class Circle(Figure):
@@ -48,10 +54,21 @@ class Circle(Figure):
 
     def __init__(self, color, *sides):
         super().__init__(color, *sides)
-        self.__radius = len(self.get_sides()) / 2 * math.pi
+        self.__radius = self.get_radius(sides)
+
+    def get_radius(self, sides):
+        return sum(self.initialize_sides(sides)) / (2 * math.pi)
+
+    def set_sides(self, *new_sides):
+        if self._Figure__is_valid_sides(new_sides):
+            self.__sides = list(new_sides)
+            self.__radius = self.get_radius(new_sides)
+
+    def get_sides(self):
+        return self.__sides
 
     def get_square(self):
-        return math.pi * self.__radius ** 2
+        return math.pi * (self.__radius ** 2)
 
 
 class Triangle(Figure):
@@ -59,17 +76,10 @@ class Triangle(Figure):
 
     def __init__(self, color, *sides):
         super().__init__(color, *sides)
-        if len(sides) > 1 and len(sides) != self.sides_count:
-            self.__sides = [1] * self.sides_count
-        else:
-            self.__sides = list(sides) * self.sides_count
-
-    def get_sides(self):
-        return self.__sides
 
     def get_square(self):
         a, b, c = self.get_sides()
-        p = sum(self.get_sides()) / 2
+        p = sum((a, b, c)) / 2
         sq = math.sqrt(p * (p - a) * (p - b) * (p - c))
         return sq
 
@@ -79,18 +89,10 @@ class Cube(Figure):
 
     def __init__(self, color, *sides):
         super().__init__(color, *sides)
-        if len(sides) > 1 and len(sides) != self.sides_count:
-            self.__sides = [1] * self.sides_count
-        else:
-            self.__sides = [side for side in list(sides) if isinstance(side, int)] * self.sides_count
-
-
-    def get_sides(self):
-        return self.__sides
 
     def get_volume(self):
-        if len(self.__sides) != 0:
-            return self.__sides[0] ** 3
+        if len(self.get_sides()) != 0:
+            return self.get_sides()[0] ** 3
 
 
 if __name__ == '__main__':
@@ -99,10 +101,14 @@ if __name__ == '__main__':
     print(triangle1.get_square())
 
     circle1 = Circle((200, 200, 100), 10)
+    print(circle1._Circle__radius)
+    print(circle1.get_square())
+    circle1.set_sides(15)
+    print(circle1._Circle__radius)
+    print(circle1.get_square())
     print(circle1.get_color())
     circle1.set_color(55, 255, 77)
     print(circle1.get_color())
-    print(circle1.get_sides())
     circle1.set_sides(15)
     print(circle1.get_sides())
     print(len(circle1))
@@ -117,7 +123,11 @@ if __name__ == '__main__':
     # Проверка объёма (куба):
     print(cube1.get_volume())
 
+    print(sum(circle1._Figure__sides))
 
-
-
-
+    print(circle1.get_sides())
+    circle1.set_sides(15)
+    print(circle1._Circle__radius)
+    circle1.set_sides(10)
+    print(circle1._Circle__radius)
+    print(circle1.get_sides())
